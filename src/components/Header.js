824f20b2ac02858a56c26a962d2557e1231ea49d
blaws006 <blaws006@gmail.com>
2023-11-27 23:03:11 -0500

    Header Working

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import { 
+    Navbar, 
+    NavbarBrand, 
+    Collapse, 
+    NavbarToggler, 
+    Nav, 
+    NavItem 
+  } from 'reactstrap';
+
+  import { useState } from 'react';
+
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <Navbar dark sticky='top'  expand='md' className=''>
+            <NavbarBrand href='/' className=''>
+                <h1 className='mt-1'>Book Store App</h1>
+            </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse navbar isOpen={menuOpen} className='nav-links'> 
+                <Nav navbar >
+                    <NavItem className='px-3'>
+                        <i className='fa fa-home fa-lg' style={{ color: '#fff'}} /> Home
+                    </NavItem>
+                    <NavItem className='px-3'>
+                    <i className='fa fa-list fa-lg' style={{ color: '#fff'}} /> Directory
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
