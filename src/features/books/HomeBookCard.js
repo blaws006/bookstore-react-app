9cba355c852db5c9ea1826173cffcbbba13fece9
blaws006 <blaws006@gmail.com>
2023-12-22 08:58:42 -0500

    Successfully added cards to homepage

--- src/features/books/HomeBookCard.js
+++ src/features/books/HomeBookCard.js
@@ -5,45 +5,41 @@ import { Link } from "react-router-dom";
 
 const HomeBookCard = ({ category }) => {
   const books = useSelector(findBookByCategory(category))
-  const isLoading = useSelector((state) => state.books.isLoading)
-  const errMess = useSelector((state) => state.books.errMess)
+  console.log(books)
   const [ book ] = books;
-  console.log(book.volumeInfo.imageLinks.thumbnail)
-  if (isLoading) {
-    return (
-        <></>
-    )
-  }
-  if (errMess !== '') {
-    <>
-        <h1>{errMess}</h1>
-    </>
-  }
+    console.log()
   return (
-    <Card
-        color="info" 
-        outline
-        style={{
-            width: '12rem',
-            marginBottom: 20,
-            
-        }}
-        className="pb-2"
-    >
-        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} style={{maxHeight: 195}} />
-        <CardBody style={{ textAlign: 'center' }}>
-            <CardTitle tag='h5' className="clipLongTitle">
-                <strong>{book.volumeInfo.title}</strong>
-            </CardTitle>
-            <CardSubtitle tag='h6' className="clipAuthor mb-3">
-                {book.volumeInfo.authors.join(', ')}
-            </CardSubtitle>
-            <Link to={`/category/${category}/books/${book.volumeInfo.title}`}>
-                <Button color="info" style={{ color: '#fff', fontWeight: 'bold' }}>More Info</Button>
-            </Link>
-        </CardBody>
-        
-    </Card>
+   <>
+    {books.map((book, idx) => {
+        return (
+                    <Card
+                color="info" 
+                outline
+                style={{
+                    width: '12rem',
+                    marginBottom: 20,
+                    
+                }}
+                className="pb-2"
+            >
+                <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} style={{maxHeight: 195}} />
+                <CardBody style={{ textAlign: 'center' }}>
+                    <CardTitle tag='h5' className="clipLongTitle">
+                        <strong>{book.volumeInfo.title}</strong>
+                    </CardTitle>
+                    <CardSubtitle tag='h6' className="clipAuthor mb-3">
+                        {book.volumeInfo.authors.join(', ')}
+                    </CardSubtitle>
+                    <Link to={`/category/${category}/books/${book.volumeInfo.title}`}>
+                        <Button color="info" style={{ color: '#fff', fontWeight: 'bold' }}>More Info</Button>
+                    </Link>
+                </CardBody>
+                
+            </Card>
+        )
+    })}
+   </>
+    
 )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,40 @@ import { categories } from "../app/shared/categories";
 // import { Link } from 'react-router-dom';
 import SubHeader from "../components/SubHeader";
 import { Row, Container } from 'reactstrap'
-import  HomeBookCard  from "../features/books/HomeBookCard";
-
+import { useSelector } from "react-redux";
+import { lazy, Suspense } from "react";
+import Loading from '../components/Loading' 
+const HomeBookCard = lazy(() => delayForDemo(import('../features/books/HomeBookCard')))
 
 const Home = () => {
-   
-
+    const booksArr = useSelector((state) => state.books.booksArray)
+    const isLoading = useSelector((state) => state.books.isLoading)
+    const errMess = useSelector((state) => state.books.errMess)
+    if (isLoading) {
+        return (
+            <>
+            <Container>
+                <Row>
+                    <SubHeader current='HOME' />
+                </Row>
+            </Container>
+                <h1>Loading</h1>
+            </>
+        )
+      }
+      if (errMess !== '') {
+        return(
+            <>
+            <Container>
+                <Row>
+                    <SubHeader current='HOME' />
+                </Row>
+            </Container>
+            <h1>{errMess}</h1>
+        </>
+        )
+        
+      }
     return (
         <>
             <Container>
@@ -15,21 +43,31 @@ const Home = () => {
                     <SubHeader current='HOME' />
                 </Row>
             </Container>
-               
-            {categories.map((category, idx) => {
-                 
+            {booksArr && categories.map((category, idx) => {
                     return (
-                        <div id={idx} key={idx} className="text-center">
-                            <HomeBookCard category={category} />
-                             {/* <Link to={`/category/${category}`}>
-                                {category.toUpperCase()}
-                            </Link>  */}
-                        </div>
-                    )
-            })}
+                    <div id={idx} key={idx} className="text-center">
+                       <Suspense fallback={<Loading />}>
+                        <HomeBookCard category={category} />
+                       </Suspense>
+                       
+                        
+                        {/* <Link to={`/category/${category}`}>
+                            {category.toUpperCase()}
+                        </Link>  */}
+                    </div>
+                )
+        })}
         </>
     )
 
 }
+
+// Add a fixed delay so you can see the loading state
+function delayForDemo(promise) {
+    return new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    }).then(() => promise);
+  }
+  
 
 export default Home;
