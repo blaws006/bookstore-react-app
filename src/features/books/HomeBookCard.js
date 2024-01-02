fb0ca1dffb2f823942251c029a537ee630a738ea
blaws006 <blaws006@gmail.com>
2024-01-02 09:36:49 -0500

    Cleaned up the home page

--- src/features/books/HomeBookCard.js
+++ src/features/books/HomeBookCard.js
@@ -1,51 +1,69 @@
 import { useSelector } from 'react-redux';
 import { findBookByCategory } from './booksSlice';
-import { Card, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    Row, 
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const HomeBookCard = ({ category }) => {
-  const books = useSelector(findBookByCategory(category));
-  console.log(books);
+    const books = useSelector(findBookByCategory(category));
+    console.log(books);
 
-  return (
-    <>
-      {books.map((book, idx) => {
-        return (
-          <Card
-            color='info'
-            outline
-            style={{
-              width: '12rem',
-              marginBottom: 20,
-            }}
-            className='pb-2'
-          >
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt={book.volumeInfo.title}
-              style={{ maxHeight: 195 }}
-            />
-            <CardBody style={{ textAlign: 'center' }}>
-              <CardTitle tag='h5' className='clipLongTitle'>
-                <strong>{book.volumeInfo.title}</strong>
-              </CardTitle>
-              <CardSubtitle tag='h6' className='clipAuthor mb-3'>
-                {book.volumeInfo.authors.join(', ')}
-              </CardSubtitle>
-              <Link to={`/category/${category}/books/${book.volumeInfo.title}`}>
-                <Button
-                  color='info'
-                  style={{ color: '#fff', fontWeight: 'bold' }}
-                >
-                  More Info
-                </Button>
-              </Link>
-            </CardBody>
-          </Card>
-        );
-      })}
-    </>
-  );
+    return (
+        <Row style={{justifyContent: 'center'}}>
+          
+                <h1><Link to={`/category/${category}`} className='homePageTitle'>{category.toUpperCase()}</Link></h1>
+                {books.filter((book, idx) => idx < 5).map((book, idx) => {
+                    return (
+                        <Card
+                            color='info'
+                            outline
+                            style={{
+                                width: '12rem',
+                                marginBottom: 20,
+                            }}
+                            className='pb-2'
+                        >
+                            <img
+                                src={book.volumeInfo.imageLinks.thumbnail}
+                                alt={book.volumeInfo.title}
+                                style={{ maxHeight: 195 }}
+                            />
+                            <CardBody style={{ textAlign: 'center' }}>
+                                <CardTitle tag='h5' className='clipLongTitle'>
+                                    <strong>{book.volumeInfo.title}</strong>
+                                </CardTitle>
+                                <CardSubtitle
+                                    tag='h6'
+                                    className='clipAuthor mb-3'
+                                >
+                                    {book.volumeInfo.authors.join(', ')}
+                                </CardSubtitle>
+                                <Link
+                                    to={`/category/${category}/books/${book.volumeInfo.title}`}
+                                >
+                                    <Button
+                                        color='info'
+                                        style={{
+                                            color: '#fff',
+                                            fontWeight: 'bold',
+                                        }}
+                                    >
+                                        More Info
+                                    </Button>
+                                </Link>
+                            </CardBody>
+                        </Card>
+                    );
+                })}
+          
+        </Row>
+    );
 };
 
 export default HomeBookCard;
--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -5,72 +5,89 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
 
 const BookPage = () => {
-  const { title, category } = useParams();
-  const book = useSelector(getSingleBook(title));
-  console.log(category);
-  return (
-    <Container
-      style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 100 + '%',
-        marginTop: 50,
-      }}
-    >
-      <Row>
-        {title && (
-          <SubHeader
-            current={title.toUpperCase()}
-            detail={true}
-            category={category}
-          />
-        )}
-      </Row>
-      <Row sm='1' md='2'>
-        <Col style={{ flexDirection: 'column', display: 'flex' }} lg='4'>
-          <Row>
-            <Col>
-              <img
-                src={book.volumeInfo.imageLinks.thumbnail}
-                alt={book.volumeInfo.title}
-                title={book.volumeInfo.title}
-                style={{ maxWidth: 128 }}
-              />
-            </Col>
-          </Row>
-          <Row style={{ marginTop: 20 }}>
-            <Col>
-              <a
-                href={book.volumeInfo.infoLink}
-                target='_blank'
-                rel='noreferrer'
-              >
-                <Button
-                  color='info'
-                  className='book-text'
-                  style={{ color: '#fff', fontWeight: 'bold' }}
+    const { title, category } = useParams();
+    const book = useSelector(getSingleBook(title));
+    console.log(category);
+    return (
+        <Container
+            style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 100 + '%',
+                marginTop: 50,
+            }}
+        >
+            <Row>
+                {title && (
+                    <SubHeader
+                        current={title.toUpperCase()}
+                        detail={true}
+                        category={category}
+                    />
+                )}
+            </Row>
+            <Row sm='1' md='2'>
+                <Col
+                    style={{ flexDirection: 'column', display: 'flex' }}
+                    lg='4'
                 >
-                  Find Book
-                </Button>
-              </a>
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <h1>{book.volumeInfo.title}</h1>
-          <p>Author: {book.volumeInfo.authors.join(', ')}</p>
-          <p>Genres: {book.volumeInfo.categories.join(', ')}</p>
-          {book.volumeInfo.averageRating && (
-            <p>
-              Average Rating among readers: {book.volumeInfo.averageRating} out
-              of 5
-            </p>
-          )}
-          <p className='text-overflow-clamp'>{book.volumeInfo.description}</p>
-        </Col>
-      </Row>
-    </Container>
-  );
+                    <Row>
+                        <Col>
+                            <img
+                                src={book.volumeInfo.imageLinks.thumbnail}
+                                alt={book.volumeInfo.title}
+                                title={book.volumeInfo.title}
+                                style={{ maxWidth: 128 }}
+                            />
+                        </Col>
+                    </Row>
+                    <Row style={{ marginTop: 20 }}>
+                        <Col>
+                            <a
+                                href={book.volumeInfo.infoLink}
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <Button
+                                    color='info'
+                                    className='book-text'
+                                    style={{
+                                        color: '#fff',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    Find Book
+                                </Button>
+                            </a>
+                        </Col>
+                    </Row>
+                </Col>
+                {book.volumeInfo && (
+                    <Col>
+                        <h1>{book.volumeInfo.title}</h1>
+                        {book.volumeInfo.authors && (
+                            <p>Author: {book.volumeInfo.authors.join(', ')}</p>
+                        )}
+                        {book.volumeInfo.categories && (
+                            <p>
+                                Genres: {book.volumeInfo.categories.join(', ')}
+                            </p>
+                        )}
+
+                        {book.volumeInfo.averageRating && (
+                            <p>
+                                Average Rating among readers:{' '}
+                                {book.volumeInfo.averageRating} out of 5
+                            </p>
+                        )}
+                        <p className='text-overflow-clamp'>
+                            {book.volumeInfo.description}
+                        </p>
+                    </Col>
+                )}
+            </Row>
+        </Container>
+    );
 };
 
 export default BookPage;
